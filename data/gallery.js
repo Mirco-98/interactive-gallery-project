48a69c99d382ccf033aaf87604f2894f91f58d4e
Mirco-98 <mirco.azzi@gmail.com>
2024-03-08 16:15:50 +0100

    Updated gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -1,31 +1,31 @@
 export const gallery = [
   {
     id: 'de754979-e8f2-46e6-85e2-b8830388e235',
-    src: '../images/1000_F_243259090_crbVsAqKF3PC2jk2eKiUwZHBPH8Q6y9Y.jpg'
+    src: 'images/1000_F_243259090_crbVsAqKF3PC2jk2eKiUwZHBPH8Q6y9Y.jpg'
   },
   {
     id: '01a07177-c001-4a57-b146-49c15d3604ef',
-    src: '../images/2021_25_deep_sea-scaled.jpg'
+    src: 'images/2021_25_deep_sea-scaled.jpg'
   },
   {
     id: '870fb152-6bde-4524-9ba8-1e89fb7d44d5',
-    src: '../images/cat-close-up-of-side-profile.webp'
+    src: 'images/cat-close-up-of-side-profile.webp'
   },
   {
     id: '8a49059e-7b7d-4b46-b3a2-6ab64dba4616',
-    src: '../images/D30I1244-v2-copy-16x9-1-scaled.jpg'
+    src: 'images/D30I1244-v2-copy-16x9-1-scaled.jpg'
   },
   {
     id: '7a9d2995-7c6f-4cc0-a96d-427e83e9051f',
-    src: '../images/get-inspired-wildlife-photography-tips-1-16.jpg'
+    src: 'images/get-inspired-wildlife-photography-tips-1-16.jpg'
   },
   {
     id: '0e6cf971-30fe-4705-8a07-d5d2a0809ecf',
-    src: '../images/GettyImages-534318658.webp'
+    src: 'images/GettyImages-534318658.webp'
   },
   {
     id: '41790747-dd6d-4772-a8a9-58b5cde31c34',
-    src: '../images/lago-di-resia.jpg'
+    src: 'images/lago-di-resia.jpg'
   },
   {
     id: '2ec79a53-5148-48bd-9200-a91b68c28365',
