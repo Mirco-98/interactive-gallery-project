4d781d95c156efb55c4c6e521d747557a1e0fea1
Mirco-98 <mirco.azzi@gmail.com>
2024-03-08 15:47:34 +0100

    Created JSON file for gallery array and made ajax request into script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,31 @@
 // import gallery array
 import { gallery } from "../data/gallery.js";
 
-// create variable to store the images
-let galleryHTML = '';
+//ajax request to get galler array 
+const xhr = new XMLHttpRequest();
 
-// loop through gallery array 
-gallery.forEach((image) => {
-  galleryHTML += `
-    <li id="img-container" class="list-group-item col-4 col-md-3 col-lg-2">
-      <img src="${image.src}" class="img-fluid">
-    </li>
-  `;
-});
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    if(xhr.status === 200) {
+      let galleryHTML = "";
 
-// generate html <li> items into the <ul>
-document.querySelector('.js-row-img-container')
-  .innerHTML = galleryHTML;
+      // loop through gallery array 
+      gallery.forEach((image) => {
+        galleryHTML += `
+          <li id="img-container" class="list-group-item col-4 col-md-3 col-lg-2">
+            <img src="${image.src}" class="img-fluid">
+          </li>
+        `;
+      });
 
+      // generate html <li> items into the <ul>
+      document.querySelector('.js-row-img-container')
+        .innerHTML = galleryHTML;
+    } else {
+      console.log('There was a problem with the request');
+    };
+  };
+};
+
+xhr.open('GET', "../backend/gallery.json");
+xhr.send();
